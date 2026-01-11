0b7b09435c3ce886089d350e10c70a92eb24cec3
Bilal Houari <bilal.bm.houari@gmail.com>
2026-01-11 17:59:16 +0100

    feat: added factories and seeding

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Resource extends Model
 {
+    use HasFactory;
+
     protected $fillable = ["name", "category", "specs", "status"];
 
     protected $casts = [
