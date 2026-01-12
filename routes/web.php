414f3288b657a7269423e49a6cce95fd2d2eb871
sacha ayoub <ayoub.sacha@etu.uae.ac.ma>
2026-01-12 20:14:16 +0000

    routage

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CatalogController;
+
+Route::get('/Catalog', [CatalogController::class, 'index']);
+
 Route::get('/', function () {
     return view('welcome');
 });
